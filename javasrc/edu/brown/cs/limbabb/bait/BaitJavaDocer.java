88fe4979a195ce46b2d2e2a6dcb62dff640eea84
Steven Reiss <spr@cs.brown.edu>
2026-05-14 13:30:48 -0400

    Fixes for javadoc insertion.

--- javasrc/edu/brown/cs/limbabb/bait/BaitJavaDocer.java
+++ javasrc/edu/brown/cs/limbabb/bait/BaitJavaDocer.java
@@ -23,6 +23,8 @@
 package edu.brown.cs.limbabb.bait;
 
 import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.text.BadLocationException;
 
@@ -31,6 +33,7 @@ import org.w3c.dom.Element;
 import edu.brown.cs.bubbles.bale.BaleConstants;
 import edu.brown.cs.bubbles.bale.BaleFactory;
 import edu.brown.cs.bubbles.board.BoardLog;
+import edu.brown.cs.bubbles.bump.BumpClient;
 import edu.brown.cs.bubbles.bump.BumpLocation;
 import edu.brown.cs.ivy.file.IvyLog;
 import edu.brown.cs.ivy.mint.MintConstants.CommandArgs;
@@ -48,10 +51,7 @@ class BaitJavaDocer implements BaitConstants, BaitConstants.ResponseHandler
 /********************************************************************************/
 
 private BumpLocation bump_location;
-private String prior_javadoc;
-private String method_contents;
-private int prior_start;
-private int prior_end;
+private ItemScanner item_scan;
 private String method_types;
 
 
@@ -65,12 +65,8 @@ private String method_types;
 BaitJavaDocer(BumpLocation loc,String types)
 {
    bump_location = loc;
-   prior_javadoc = null;
-   method_contents = null;
-   prior_start = -1;
-   prior_end = -1;
    method_types = types;
-   scanMethod();
+   item_scan = new ItemScanner(loc);
 }
 
 
@@ -85,10 +81,10 @@ void process()
    String body = null;
    try (IvyXmlWriter xw = new IvyXmlWriter()) {
       xw.begin("JAVADOC");
-      if (prior_javadoc != null && !prior_javadoc.isEmpty()) {
-         xw.cdataElement("PRIOR",prior_javadoc);
+      if (item_scan.getJavadoc() != null && !item_scan.getJavadoc().isEmpty()) {
+         xw.cdataElement("PRIOR",item_scan.getJavadoc());
        }
-      xw.cdataElement("CODE",method_contents);
+      xw.cdataElement("CODE",item_scan.getContents());
       xw.end("JAVADOC");
       body = xw.toString();
     }
@@ -120,109 +116,45 @@ void process()
 
 @Override public void handleResponse(Element rslt)
 {
+   List<Element> todo = new LinkedList<>();
+   for (Element jelt : IvyXml.children(rslt,"JDOC")) {
+      todo.add(0,jelt);
+    }
+   
    if (method_types == null) {
       // handle single method and single javadoc
-      String jdoc = IvyXml.getTextElement(rslt,"JDOC"); 
-      if (jdoc != null && !jdoc.isEmpty()) {
-         replaceJavaDoc(jdoc);
+      if (todo.size() == 1) {
+         String jdoc = IvyXml.getTextElement(rslt,"JDOC"); 
+         if (jdoc != null && !jdoc.isEmpty()) {
+            replaceJavaDoc(jdoc,item_scan);
+          }
        }
     }
    else if (method_types.equals("*")) {
       // handle single class
-      BoardLog.logD("BAIT","Handle class javadoc");
+      if (todo.size() == 1) {
+         String jdoc = IvyXml.getTextElement(rslt,"JDOC"); 
+         if (jdoc != null && !jdoc.isEmpty()) {
+            replaceJavaDoc(jdoc,item_scan);
+          }
+       }
     }
    else {
+      BumpClient bc = BumpClient.getBump();
+      for (Element jelt : todo) {
+         String name = IvyXml.getAttrString(jelt,"NAME");
+         String jdoc = IvyXml.getText(jelt);
+         if (jdoc == null || jdoc.isEmpty()) continue;
+         List<BumpLocation> locs = bc.findMethod(bump_location.getProject(),name,false);
+         for (BumpLocation loc : locs) {
+            if (loc.getFile() != bump_location.getFile()) continue;
+            ItemScanner lscn = new ItemScanner(loc);
+            // check for location already done?
+            replaceJavaDoc(jdoc,lscn);
+          }
+       }
       BoardLog.logD("BAIT","Handle multiple method javadoc");
     }
-}
-
-
-/********************************************************************************/
-/*                                                                              */
-/*      Get the method body and prior JavaDoc                                   */
-/*                                                                              */
-/********************************************************************************/
-
-private void scanMethod()
-{
-   File f = bump_location.getFile();
-   String proj = bump_location.getProject();
-   BaleConstants.BaleFileOverview bfo = BaleFactory.getFactory().
-         getFileOverview(proj,f);
-   String code = null;
-   try {
-      code = bfo.getText(bump_location.getDefinitionOffset(),
-	    bump_location.getDefinitionEndOffset() - bump_location.getDefinitionOffset());
-    }
-   catch (BadLocationException e) {
-      BoardLog.logE("BAIT","Problem getting text for method",e);
-      return;
-    }
-   
-   int x1 = -1; 
-   StringBuffer buf = new StringBuffer();
-   StringBuffer jdoc = null;
-   boolean inlinecmmt = false;
-   boolean inareacmmt = false;
-   char lastchar = 0;
-   int havetext = 0;
-   
-   for (int i = 0; i < code.length(); ++i) {
-      char ch = code.charAt(i);
-      if (inlinecmmt) { 			// check for end of comment
-	 if (ch == '\n') {
-	    inlinecmmt = false;
-          }
-       }
-      else if (inareacmmt) {
-         if (jdoc != null) {
-            jdoc.append(ch);
-            if (Character.isLetterOrDigit(ch)) ++havetext;
-          }
-	 if (lastchar == '*' && ch == '/') {
-	    inareacmmt = false;
-            if (jdoc != null) {
-               if (havetext > 10) {
-                  buf.append(jdoc);
-                  buf.append("\n");
-                  prior_end = bump_location.getDefinitionOffset() + i + 1;
-                }
-               else if (buf.isEmpty()) {
-                  prior_start = -1;
-                }
-               jdoc = null;
-               havetext = 0;
-            }
-          }
-       }
-     
-      if (inareacmmt || inlinecmmt) ;
-      else if (Character.isLetterOrDigit(ch)) {
-         if (x1 < 0) x1 = i;
-         break;
-       }
-      else if (lastchar == '/') {
-         if (ch == '/') {
-            inlinecmmt = true;
-          }
-         else if (ch == '*') {
-            if (i+1 < code.length() && code.charAt(i+1) == '*') {
-               havetext = 0;
-               if (prior_start < 0) {
-                  prior_start = bump_location.getDefinitionOffset() + i - 1;
-                }
-               jdoc = new StringBuffer();
-               jdoc.append("/*");
-             }
-            inareacmmt = true;
-          }
-       }
-      lastchar = ch;
-    }
-   if (!buf.isEmpty()) prior_javadoc = buf.toString();
-   if (x1 < 0) x1 = 0;
-   method_contents = code.substring(x1);
-   IvyLog.logD("LIMBA","Found prior " + prior_start + " " + prior_end);
 }
 
 
@@ -232,7 +164,7 @@ private void scanMethod()
 /*                                                                              */
 /********************************************************************************/
 
-void replaceJavaDoc(String jdoc)
+void replaceJavaDoc(String jdoc,ItemScanner scan)
 {
    // might need to get bump location again and then scan it again
    File f = bump_location.getFile();
@@ -242,17 +174,134 @@ void replaceJavaDoc(String jdoc)
    int start = bump_location.getDefinitionOffset();
    
    try {
-      if (prior_start < 0 || prior_end < 0) {
+      if (scan.getPriorStart() < 0 || scan.getPriorEnd() < 0) {
          bfo.insertString(start,jdoc,null);
        }
       else {
-         bfo.replace(prior_start,prior_end-prior_start,jdoc,false,false);
+         bfo.replace(scan.getPriorStart(),
+               scan.getPriorEnd()-scan.getPriorStart(),
+               jdoc,false,false);
        }
     }
    catch (BadLocationException e) {
       BoardLog.logE("BAIT","Problem inserting javadoc",e);
     }
 }
+
+
+
+/********************************************************************************/
+/*                                                                              */
+/*      Get the method body and prior JavaDoc                                   */
+/*                                                                              */
+/********************************************************************************/
+
+
+private static final class ItemScanner {
+   
+   private BumpLocation bump_location;
+   private String prior_javadoc;
+   private String item_contents;
+   private int prior_start;
+   private int prior_end;
+   
+   
+   ItemScanner(BumpLocation loc) {
+      bump_location = loc;
+      prior_javadoc = null;
+      item_contents = null;
+      prior_start = -1;
+      prior_end = -1;
+      scanMethod();
+    }
+   
+   String getJavadoc()                          { return prior_javadoc; }
+   int getPriorStart()                          { return prior_start; }
+   int getPriorEnd()                            { return prior_end; }
+   String getContents()                         { return item_contents; }
+   
+   private void scanMethod() {
+      File f = bump_location.getFile();
+      String proj = bump_location.getProject();
+      BaleConstants.BaleFileOverview bfo = BaleFactory.getFactory().
+            getFileOverview(proj,f);
+      String code = null;
+      try {
+         code = bfo.getText(bump_location.getDefinitionOffset(),
+               bump_location.getDefinitionEndOffset() - bump_location.getDefinitionOffset());
+       }
+      catch (BadLocationException e) {
+         BoardLog.logE("BAIT","Problem getting text for method",e);
+         return;
+       }
+      
+      int x1 = -1; 
+      StringBuffer buf = new StringBuffer();
+      StringBuffer jdoc = null;
+      boolean inlinecmmt = false;
+      boolean inareacmmt = false;
+      char lastchar = 0;
+      int havetext = 0;
+      
+      for (int i = 0; i < code.length(); ++i) {
+         char ch = code.charAt(i);
+         if (inlinecmmt) { 			// check for end of comment
+            if (ch == '\n') {
+               inlinecmmt = false;
+             }
+          }
+         else if (inareacmmt) {
+            if (jdoc != null) {
+               jdoc.append(ch);
+               if (Character.isLetterOrDigit(ch)) ++havetext;
+             }
+            if (lastchar == '*' && ch == '/') {
+               inareacmmt = false;
+               if (jdoc != null) {
+                  if (havetext > 10) {
+                     buf.append(jdoc);
+                     buf.append("\n");
+                     prior_end = bump_location.getDefinitionOffset() + i + 1;
+                   }
+                  else if (buf.isEmpty()) {
+                     prior_start = -1;
+                   }
+                  jdoc = null;
+                  havetext = 0;
+                }
+             }
+          }
+         
+         if (inareacmmt || inlinecmmt) ;
+         else if (Character.isLetterOrDigit(ch) || ch == '@') {
+            if (x1 < 0) x1 = i;
+            break;
+          }
+         else if (lastchar == '/') {
+            if (ch == '/') {
+               inlinecmmt = true;
+             }
+            else if (ch == '*') {
+               if (i+1 < code.length() && code.charAt(i+1) == '*') {
+                  havetext = 0;
+                  if (prior_start < 0) {
+                     prior_start = bump_location.getDefinitionOffset() + i - 1;
+                   }
+                  jdoc = new StringBuffer();
+                  jdoc.append("/*");
+                }
+               inareacmmt = true;
+             }
+          }
+         lastchar = ch;
+       }
+      if (!buf.isEmpty()) prior_javadoc = buf.toString();
+      if (x1 < 0) x1 = 0;
+      item_contents = code.substring(x1);
+      IvyLog.logD("LIMBA","Found prior " + prior_start + " " + prior_end);
+    }
+   
+}       // end of inner class ItemScanner
 
 
 }       // end of class BaitJavaDocer
